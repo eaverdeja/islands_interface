e94be4832f0a3fdcd756de6bf34669fbb1bf13b1
Eduardo Verdeja <eaverdeja@gmail.com>
2019-08-17 18:04:14 -0300

    Updating test configs

--- config/test.exs
+++ config/test.exs
@@ -1,5 +1,14 @@
 use Mix.Config
 
+# Configure your database
+config :islands_interface, IslandsInterface.Repo,
+  username: "postgres",
+  password: "postgres",
+  database: "islands_interface_test",
+  hostname: "localhost",
+  show_sensitive_data_on_connection_error: true,
+  pool: Ecto.Adapters.SQL.Sandbox
+
 # We don't run a server during test. If one is required,
 # you can enable the server option below.
 config :islands_interface, IslandsInterfaceWeb.Endpoint,
--- test/support/conn_case.ex
+++ test/support/conn_case.ex
@@ -26,7 +26,13 @@ defmodule IslandsInterfaceWeb.ConnCase do
     end
   end
 
-  setup _tags do
+  setup tags do
+    :ok = Ecto.Adapters.SQL.Sandbox.checkout(IslandsInterface.Repo)
+
+    unless tags[:async] do
+      Ecto.Adapters.SQL.Sandbox.mode(IslandsInterface.Repo, {:shared, self()})
+    end
+
     {:ok, conn: Phoenix.ConnTest.build_conn()}
   end
 end
